228a52c35916271c82db7b9b941924e441d596ff
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2021-06-15 18:00:45 +0800

    Chatting

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ wsServer.on('request', function(request) {
 
     connection.on('message', function(message) {
         console.log('Received Message:', message.utf8Data);
-        connection.sendUTF('Hi this is WebSocket server!');
+        connection.sendUTF(message.utf8Data)
     });
     connection.on('close', function(reasonCode, description) {
         console.log('Client has disconnected.');
